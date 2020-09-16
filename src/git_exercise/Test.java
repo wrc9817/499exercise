293d641b8b6c7de11b019f05d42a23b2593a6880
unknown <wrc9817@gmail.com>
2020-09-16 08:56:47 -0700

    main file

--- src/git_exercise/Test.java
+++ src/git_exercise/Test.java
@@ -5,8 +5,11 @@ public class Test {
 		// test whether the area calculations are correct 
 		//by comparing circles and rectangles' Area.
 		circle c = new circle(2);
+		// c's area is 2*2*pi
 		rectangle r = new rectangle(4, 3.14);
+		//r's area is 4*pi
 		rectangle r1 = new rectangle(4, 5);
+		//r1's area is 4*5
 		if(c.getArea()==r.getArea()) {
 			System.out.print("Equal\r");
 		}else {
